d1f76284463e24773571ba043441e1d2f6a09645
mosbasaumuoi <mos360.vn@gmail.com>
2026-05-29 14:56:28 +0700

    Create ic3-level1.js

---
+++ questions/ic3-level1.js
@@ -0,0 +1,17 @@
+export const IC3_LEVEL1 = [
+  {
+    id: 1,
+    level: "IC3_LEVEL1",
+    type: "single",
+    question: "What does PII stand for?",
+    options: [
+      "Personally Identifiable Information",
+      "Public Internet Information",
+      "Protected Internal Index",
+      "Personal Internet Identity"
+    ],
+    answer: 0,
+    explanation:
+      "PII stands for Personally Identifiable Information."
+  }
+];
