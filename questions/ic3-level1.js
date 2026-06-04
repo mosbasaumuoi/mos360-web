f779e91f7b402f95d78dee6ccc9b9196bdf7e738
Phạm Thế Cường <mos360.vn@gmail.com>
2026-06-04 10:13:10 +0700

    fix trở lại

--- questions/ic3-level1.js
+++ questions/ic3-level1.js
@@ -197,7 +197,7 @@ export const IC3_LEVEL1 = [
     level: "IC3_LEVEL1",
     test: 1,
     category: "OPERATING_SYSTEM",
-    image_key: "ic3_lv1_q14_opt_a",
+    image_key: "ic3_lv1_q10_operating_system",
     difficulty: "medium",
     type: "matching",
     question: "Bạn cần xác định các chức năng do Hệ điều hành của máy tính quản lý. Với mỗi tác vụ dưới đây, hãy chọn Đúng hoặc Sai.",
